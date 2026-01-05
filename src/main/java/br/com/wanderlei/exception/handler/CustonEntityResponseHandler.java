e613d8f7cc49721364c824e7fc131b51bb289403
wanderlei jose bolzan <wbolzanjp@gmail.com>
2026-01-04 21:53:31 -0300

    Inciado o projeto Calculadora

---
+++ src/main/java/br/com/wanderlei/exception/handler/CustonEntityResponseHandler.java
@@ -0,0 +1,35 @@
+package br.com.wanderlei.exception.handler;
+
+import br.com.wanderlei.exception.ExceptionResponse;
+import br.com.wanderlei.exception.UnsuportedMathOperationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+@RestController
+public class CustonEntityResponseHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler( Exception.class)
+    public final ResponseEntity<ExceptionResponse> hanleAllExceptions(Exception ex, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse (
+                new Date (  ),
+                ex.getMessage (),
+                request.getDescription (false));
+        return new ResponseEntity<> (response, HttpStatus.INTERNAL_SERVER_ERROR );
+    }
+
+    @ExceptionHandler( UnsuportedMathOperationException.class)
+    public final ResponseEntity<ExceptionResponse> hanleBadRequestExceptions(Exception ex, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse (
+                new Date (  ),
+                ex.getMessage (),
+                request.getDescription (false));
+        return new ResponseEntity<> (response, HttpStatus.BAD_REQUEST );
+    }
+}
